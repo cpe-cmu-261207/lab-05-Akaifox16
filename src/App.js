ef10194e28fadae8a2cd01ecc309c9ca52067d68
KAMONPAT SUNTHONPONG <kamonpat_s@cmu.ac.th>
2021-01-10 15:06:36 +0700

    loolk like the same but remember

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import CourseCard from "./components/CourseCard";
 import CourseForm from "./components/CourseForm";
 
+export const CardContext = createContext({});
+
 function App() {
+  const reducer = (state , action) =>{
+    switch(action.type){
+      case "ADD_CARD":
+      return {
+        ...state,
+        currentState: [...state.currentState, action.payload],
+      };
+      case "SET_CARD":
+      return {
+        ...state,
+        currentState: action.payload,
+      };
+    case "DELETE_CARD":
+      return {
+        ...state,
+        currentState: state.currentState.filter((card) => card.id !== action.payload),
+      };
+    }
+  }
+  const [state,dispatch] = useReducer(reducer, {currentState:[],})
+  function fetchCards() {
+    const localState = localStorage.getItem("currentState");
+    if (localState) {
+      dispatch({
+        type: "SET_CARD",
+        payload: JSON.parse(localState),
+      })
+    }
+  }
+
+  useEffect(fetchCards, [])
+  useEffect(() => {
+    localStorage.setItem("currentState", JSON.stringify(state.currentState));
+  }, [state.currentState])
+  
   return (
-    <div>
-      <h1>GPA CALCULATOR</h1>
-      {/* TODO ADD UI */}
-    </div>
+    <CardContext.Provider value = {{state,dispatch}}>
+      <div>
+        <h1>GPA CALCULATOR</h1>
+        <CourseCard state={state.currentState} />
+        <CourseForm />
+      </div>
+    </CardContext.Provider>
   );
 }
 
--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,9 +1,30 @@
+import { useContext, useState } from "react";
+import {CardContext} from "../App"
 const CourseCard = (props) => {
-  return (
-    <>
-      <p>This is Course card</p>
-    </>
-  );
+  const { dispatch } = useContext(CardContext);
+  return (props.state.map(obj => {
+    return (<>
+      <div class="CourseCard">
+        <tr id="CC">
+          <td>
+            <div class="tag">
+              <p>วิชา : {obj.name}</p>
+              <p>เกรด : {obj.a}</p>
+              <p>หน่วยกิต : {obj.c}</p>
+            </div>
+          </td>
+          <td>
+            <button class="btn" onClick = {()=>{
+                dispatch({
+                    type: "DELETE_CARD",
+                    payload: obj.id,
+                });}}>x</button>
+          </td>
+        </tr>
+      </div>
+    </>)
+  }))
+  
 };
 
 export default CourseCard;
